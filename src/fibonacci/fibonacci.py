947b5866dc76ea55f4cd176a021e750b61ca673d
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2023-02-18 14:05:07 +0100

    ok

--- src/fibonacci/fibonacci.py
+++ src/fibonacci/fibonacci.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 
 
+
 @lru_cache(maxsize=1000)
 def fibonacci(n: int):
     return 1 if (n == 1 or n == 2) else fibonacci(n - 1) + fibonacci(n - 2)
@@ -9,5 +10,3 @@ def fibonacci(n: int):
 if __name__ == '__main__':
     for i in range(1, 100):
         print(f"fibonacci({i}) = {fibonacci(i)}")
-
-yhv
