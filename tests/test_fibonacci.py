22a363b8d23755f1cb5ce008302af09d6c9be4ba
AlexisN3 <alexis.martin@isen.yncrea.fr>
2023-02-24 12:13:48 +0100

    test1

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 def fibonacci(n: int):
     return 1 if (n == 1 or n == 2) else fibonacci(n - 1) + fibonacci(n - 2)
 
+
 def unit_test_fibonacci():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
@@ -12,4 +13,3 @@ def unit_test_fibonacci():
     assert fibonacci(3) == 2
     assert fibonacci(4) == 3
     assert fibonacci(5) == 5
-    
